d9b81762b2ca11a23db6a0ed9a8420e6cd811457
jesse-woo <114460358+jesse-woo@users.noreply.github.com>
2023-03-30 00:02:43 -0400

    try to zip

---
+++ index-photos/lambda_function.py
@@ -0,0 +1,97 @@
+import json
+import boto3
+from opensearchpy import OpenSearch, RequestsHttpConnection
+import urllib.parse
+import datetime
+from requests_aws4auth import AWS4Auth
+
+REGION = 'us-east-1'
+HOST = "search-photos-werz5sjzmsxjmnxz2ctmkbbtiu.us-east-1.es.amazonaws.com"
+INDEX = 'photos'
+
+session = boto3.Session()
+
+def get_awsauth(region, service):
+    cred = session.get_credentials()
+    return AWS4Auth(cred.access_key,
+                    cred.secret_key,
+                    region,
+                    service,
+                    session_token=cred.token)
+
+def index_photo(b, name):
+    client = OpenSearch(hosts=[{
+        'host': HOST,
+        'port': 443
+    }],
+                        http_auth=get_awsauth(REGION, 'es'),
+                        use_ssl=True,
+                        verify_certs=True,
+                        connection_class=RequestsHttpConnection)
+
+    res = client.create(index=INDEX, body=b, id=name)
+
+def lambda_handler(event, context):
+    s3_client = boto3.client('s3')
+    rek_client = boto3.client('rekognition')
+    bucket = bucket = event['Records'][0]['s3']['bucket']['name']
+    # key_name should be the name of the photo
+    key_name = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+    
+    
+    # code adapted from https://serverlessland.com/snippets/integration-s3-to-lambda?utm_source=aws&utm_medium=link&utm_campaign=python&utm_id=docsamples
+    try:
+        response = s3_client.head_object(Bucket = bucket, Key = key_name)
+        
+        custom_labels = []
+        if response["Metadata"]:
+            print("METADATA IS:", response["Metadata"])
+            
+            if response["Metadata"]['customlabels']:
+                json_labels = json.loads(response["Metadata"]['customlabels'])
+                to_append = json_labels['labels']
+                print("the custom labels are:", to_append)
+                custom_labels = to_append
+                
+        
+        print("The custom labels are", custom_labels)
+        timestamp = str(response["LastModified"])
+        img_object = {'S3Object': {'Bucket': bucket, 'Name': key_name}}
+    
+        label_response = rek_client.detect_labels(
+            Image=img_object)
+        
+        # build list of only the labels from the response
+        labels = []
+        for l in label_response["Labels"]:
+            labels.append(l['Name'])
+        
+        print("the Rekognition labels are", labels)
+        
+        for c in custom_labels:
+            labels.append(c)
+        
+        print("Full set of labels are", labels)
+        
+        to_idx = {
+            "objectKey": key_name, 
+            "bucket": bucket, 
+            "createdTimestamp": timestamp, 
+            "labels": labels
+        }
+        
+        index = json.dumps(to_idx, default=str)
+        index_photo(index, key_name)
+        print("this is what you indexed", index)
+        
+    except Exception as e:
+        print(e)
+        print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key_name, bucket))
+        raise e
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Successfully indexed photo')
+    }
+
+
---
+++ search-photos/lambda_function.py
@@ -0,0 +1,159 @@
+import json
+import os
+import boto3
+from opensearchpy import OpenSearch, RequestsHttpConnection
+from requests_aws4auth import AWS4Auth
+from inflection import singularize
+import re
+import uuid
+
+REGION = 'us-east-1'
+HOST = 'search-photos-werz5sjzmsxjmnxz2ctmkbbtiu.us-east-1.es.amazonaws.com'
+INDEX = 'photos'
+# You modified the lambda successfully hurray!
+session = boto3.Session()
+
+def query_photos(keywords):
+    keywords = clean_keywords(keywords)
+    print("Cleaned up keywords:", keywords)
+    
+    to_query = {
+        "query": {
+            "bool": {
+                "must": [
+                    {"match": {"labels": {"query": keyword, "boost": 1.0}}}
+                    for keyword in keywords
+                ]
+            }
+        },
+        "size": 100,
+        "_source": ["objectKey", "bucket", "createdTimestamp", "labels"],
+    }
+    '''
+    # this will search with logical OR
+    to_query = {
+        "query": {
+            "bool": {
+                "minimum_should_match": 1,
+                    "should":
+                        [
+                            {"match": {"labels": {"query": keyword, "boost": 1.0}}}
+                            for keyword in keywords
+                        ]
+            }
+        },
+        "size": 100,
+        "_source": ["objectKey", "bucket", "createdTimestamp", "labels"],
+    }
+    '''
+    print("The query is", to_query)
+    client = OpenSearch(hosts=[{
+        'host': HOST,
+        'port': 443
+    }],
+                        http_auth=get_awsauth(REGION, 'es'),
+                        use_ssl=True,
+                        verify_certs=True,
+                        connection_class=RequestsHttpConnection)
+    
+    res = client.search(index=INDEX, body=to_query)
+    print("res is", res)
+
+    hits = res['hits']['hits']
+    results = []
+    for hit in hits:
+        results.append(hit['_source'])
+
+    return results
+    
+    
+def clean_keywords(keywords):
+    keywords = [
+        part.strip()
+        for keyword in keywords
+        for part in re.sub(r"(?: and | in | the | a )", ",", keyword).split(",")
+        ]
+    keywords = list(filter(bool, keywords))
+    keywords = [re.sub(r"\s+", "", keyword) for keyword in keywords]
+    keywords = [keyword.lower() for keyword in keywords]
+    keywords = [singularize(keyword) for keyword in keywords]
+  
+    return keywords
+
+
+def get_awsauth(region, service):
+    cred = session.get_credentials()
+    return AWS4Auth(cred.access_key,
+                    cred.secret_key,
+                    region,
+                    service,
+                    session_token=cred.token)
+                    
+                    
+                    
+def lex_keywords(user_str):
+    lex_client = boto3.client('lexv2-runtime', region_name=REGION)
+    if not user_str:
+        raise ValueError("No string to disambiguate")
+        
+    sess_id = str(uuid.uuid4())
+    
+    response = lex_client.recognize_text(
+        botId = "41LW6D3J1B",
+        botAliasId = "2SB4OODZ8G",
+        localeId="en_US",
+        sessionId = sess_id,
+        text = user_str
+    )
+    
+    msg = response.get("messages", [])
+    print("RESPONSE",response)
+    print("msg_from_lex",msg)
+    
+    slots = response["interpretations"][0]["intent"]["slots"]
+    print("SLOTS:",slots)
+    
+    keywords = [slot["value"]["interpretedValue"] for slot in slots.values() if slot and "value" in slot]
+    
+    return keywords
+    
+
+def lambda_handler(event, context):
+
+    user_query = event.get("q")
+    
+    cors = {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credientials": "true",
+        "Access-Control-Allow-Methods": "GET, OPTIONS, PUT",
+        "Access-Control-Allow-Headers": "Content-Type, x-amz-meta-customLabels"
+    }
+        
+    try:
+        keywords = lex_keywords(user_query)
+
+        print("Keywords:", keywords)
+        if not keywords:
+            return {
+                "statusCode": 200,
+                "headers": cors,
+                "body": "[]"
+            }
+            
+        print("You are about to query photos")
+        results = query_photos(keywords)
+    
+        return {
+            'statusCode': 200,
+            'headers': cors,
+            'body': json.dumps(results)
+        }
+    
+    except Exception as e:
+        print(e)
+        return {
+            'statusCode' : 500,
+            'headers': cors,
+            'body': json.dumps('Backend error')
+        }
+        
